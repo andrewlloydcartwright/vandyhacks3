0d7a27b083970ce0fbcf07c717b92e239c2e5ff4
Nicole Mubarak <nik4mub@uab.edu>
2016-11-12 21:29:58 -0600

    Add files via upload

---
+++ asteriod_harvest_game/enemies.py
@@ -0,0 +1,37 @@
+#Imports
+import pygame as py
+from settings import *
+
+class Mob(py.sprite.Sprite):
+    def __init__(self):
+        py.sprite.Sprite.__init__(self)
+        self.image_orig = meteor_img
+        self.image_orig.set_colorkey(BLACK)
+        self.image = self.image_orig.copy()
+        self.rect = self.image.get_rect()
+        self.radius = int(self.rect.width *.80 /2)
+        # pygame.draw.circle(self.image, RED, self.rect.center, self.radius)
+        self.rect.x = random.randrange(WIDTH - self.rect.width)
+        self.rect.y = random.randrange(-100,-40)
+        self.speedy = random.randrange(1,8)
+        self.speedx = random.randrange(-3,3)
+        self.rot = 0
+        self.rot_speed = random.randrange(-8,8)
+        self.last_update = py.time.get_ticks()
+
+    def rotate(self):
+        now = py.time.get_ticks()
+        if now - self.last_update > 50:
+            self.last_update = now
+            self.rot = (self.rot + self.rot_speed) % 360
+            self.image = py.transform.rotate(self.image_orig, self.rot)
+
+
+    def update(self):
+        self.rotate()
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+        if self.rect.top > HEIGHT + 10 or self.rect.left < -25 or self.rect.top > HEIGHT + 20 or self.rect.bottom > HEIGHT + 20 :
+            self.rect.x = random.randrange(WIDTH - self.rect.width)
+            self.rect.y = random.randrange(-100,-40)
+            self.speedy = random.randrange(1,8)
